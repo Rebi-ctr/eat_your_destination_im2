98d957d6af74cf035c8be40fd221def09432cfb2
Rebecca Baumberger <rebecca.baumberger@stud.fhgr.ch>
2025-05-07 15:41:54 +0200

    fetch api zürich und timestamp

--- js/script.js
+++ js/script.js
@@ -1,18 +1,27 @@
 console.log("Hello World!");
 
-const all_pokemon = await loadDepartureZurich();
+
+let now = Math.floor(Date.now() / 1000); // current time in Unix timestamp (seconds)
+let startTimecode = now - (1 * 60 * 60);     // 1 hours ago
+let endTimecode = now + (12 * 60 * 60);      // 12 hours from now
+
+
+const departureZurich = await loadDepartureZurich(startTimecode, endTimecode);
 
 // Fetch the data from the API
-async function loadDepartureZurich() {
-    const url = 'https://pokeapi.co/api/v2/pokemon?limit=9&offset=0'; // mit korrekter API-URL ersetzen
+async function loadDepartureZurich(startTime, endTime) {
+    const url =`https://opensky-network.org/api/flights/departure?airport=LSZH&begin=${startTime}&end=${endTime}`;
     try {
         const response = await fetch(url);
         const answer = await response.json();
-        return answer.results;
+          return answer;
     } catch (error) {
         console.error(error);
         return false;
     }
 }
 // Display the data in the console
-console.log(loadDepartureZurich);
+console.log(departureZurich);
+
+
+
